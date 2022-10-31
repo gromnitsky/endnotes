97d34aab0073be3a96ce59c481d3f1c9329011c7
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2022-10-31 13:02:11 +0200

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# endnotes
+
+A tiny, vanilla JavaScript popup footnotes library.
+
+* No dependencies
+* 3.5KB unminified
+
+Demo: https://gromnitsky.users.sourceforge.net/js/examples/endnotes/
+
+![](example.png)
+
+## Usage
+
+html:
+
+~~~
+<p>
+  this requires an explanation <a class="footnote" href="#foo">1</a>
+</p>
+
+<p id="foo">
+  hello
+</p>
+~~~
+
+js:
+
+~~~
+<script type="module">
+  import footnotes from './endnotes.js'
+  footnotes('a.footnote')
+</script>
+~~~
+
+This adds event listeners to everything that matches `a.footnote`
+selector. When a user hovers over a link, it extracts inner html from
+the place to which the link's href points & displays a popup.
+
+The latter implies a local source of data, but you can provide your own
+function for the data extraction:
+
+    footnotes('a.footnote', {ref: myfunc})
+
+`myfunc` will get 1 argument: a value of `href` attribute of an
+hovered element.
+
+Other possible options: `width` & `height` for the popup window.
+
+## License
+
+MIT
---
+++ endnotes.js
@@ -0,0 +1,116 @@
+let opt = {
+    width: 300,
+    height: 150,
+    ref: href => {
+        let hash = new URL(href).hash
+        let ref = document.querySelector(hash)
+        return ref?.innerHTML || 'invalid ref'
+    }
+}
+
+let id = {
+    wrapper: "footnotes_dialog_12c1b560",
+    get content() { return this.wrapper + '_content' }
+}
+
+export default function(css_selector, options) {
+    Object.assign(opt, options)
+    document.querySelectorAll(css_selector).forEach( node => {
+        node.addEventListener('mouseover', is_mobile() ? dialog_create : debounce(dialog_create))
+        node.addEventListener('mouseout', dialog_remove)
+        node.addEventListener('click', e => e.preventDefault())
+    })
+}
+
+function dialog_create(event) {
+    // don't show the dialog if a user just quickly moved a mouse
+    // cursor over a link
+    if (!event.target.matches(':hover')) return
+
+    let width = opt.width
+    let padding = 16
+    let border = 1
+    let scrollbar = 16 // a guess
+    let width_total = width + padding*2 + border*2 + scrollbar
+    let height = opt.height
+    let trasparent_top_margin = 20
+    let height_total = height + padding*2 + border*2 + trasparent_top_margin
+    let x = event.x - width_total/2
+    let y = event.y
+
+    // this div is transparent
+    let wrapper = document.createElement('div')
+    wrapper.id = id.wrapper
+    wrapper.style.position = 'fixed'
+    wrapper.style.left = `${x}px`
+    wrapper.style.top =`${y}px`
+//    wrapper.style.outline = '1px solid red'
+
+    /* wrapper inner div is not transparent
+
+       +-------------+
+       | transparent |
+       |+-----------+|
+       ||           ||
+       ||   dlg     ||
+       ||           ||
+       |+-----------+|
+       +-------------+
+    */
+    wrapper.innerHTML = `<div id="${id.content}"></div>`
+    let dlg = wrapper.querySelector('#' +id.content)
+    dlg.style.marginTop = trasparent_top_margin + 'px'
+    dlg.style.border = `1px solid lightgray`
+    dlg.style.boxShadow = 'rgba(100, 100, 111, 0.5) 0px 7px 14px 0px'
+    dlg.style.borderRadius = '5px'
+    dlg.style.background = 'white'
+    dlg.style.padding = padding + 'px'
+    dlg.style.overflowY = 'auto'
+    dlg.style.width = width + 'px'
+    dlg.style.height = height + 'px'
+
+    // fix coordinates if a mouse cursor is too close to the viewport edges
+    if (x < 0) wrapper.style.left = '0px'
+    if (window.innerWidth - x < width_total)
+        wrapper.style.left = (window.innerWidth - width_total) + 'px'
+    if (window.innerHeight - y < height_total)
+        wrapper.style.top = (window.innerHeight - height_total) + 'px'
+
+    dlg.innerHTML = opt.ref(event.target.href)
+
+    wrapper.addEventListener('mouseout', dialog_remove2)
+    document.querySelector('body').appendChild(wrapper)
+}
+
+function dialog_remove() {
+    let dlg = document.querySelector('#' + id.wrapper)
+    if (!dlg || dlg.matches(':hover')) return
+
+    document.querySelectorAll('#' + id.wrapper).forEach( div => {
+        div.remove()
+    })
+}
+
+function dialog_remove2(event) {
+    document.querySelectorAll('#' + id.wrapper).forEach( div => {
+        if (event?.relatedTarget?.closest('#' + id.content)) {
+            // do nothing: mouseout event was fired because a cursor
+            // entered a child element (<a> for example) withing the
+            // dialog
+        } else {
+            div.remove()
+        }
+    })
+}
+
+function debounce(fn, ms = 250) {
+    let timeout_id
+    return function(...args) {
+        clearTimeout(timeout_id)
+        timeout_id = setTimeout(() => fn.apply(this, args), ms)
+    }
+}
+
+function is_mobile() {
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+}
